57e06bf4da73e308e46c1564cc23a2cc77621912
safitrih <miraayusaftri@gmail.com>
2022-12-27 17:52:13 +0700

    menambah app products,services

--- mira/mira/urls.py
+++ mira/mira/urls.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('products/', include ('products.urls')),
+    path('services/', include ('services.urls')),
+     path('about/', include ('about.urls')),
 ]
