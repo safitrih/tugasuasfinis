4e8184cb5602bb3c77dee8a2936e4fc662f116cb
safitrih <miraayusaftri@gmail.com>
2022-12-27 19:39:22 +0700

    membuat super user dan tabel pada app

--- mira/services/models.py
+++ mira/services/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class layanan(models.Model):
+    platfrom = models.CharField(max_length=50)
+
+    berupa = models.CharField(max_length=50)
+
